0bb263bb446a1ef3f72aa20840ad892d42262ff0
Rishit Patel <prishit547@gmail.com>
2026-07-14 03:34:39 +0530

    Update clinic contact information: change email and fax number

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -13,8 +13,8 @@ export const CLINIC = {
   name: "Socal Family Eye Care",
   phone: "562-988-2020",
   phoneHref: "tel:5629882020",
-  fax: "562-988-2022",
-  email: "info@socalfamilyeye.com",
+  fax: "562-426-7394",
+  email: "frontdesk@socalfamilyeyecare.com",
   address: "3650 Atlantic Ave, Long Beach, CA 90807",
   mapUrl: "https://maps.app.goo.gl/6288XXpFoBrncHxj9",
 };
